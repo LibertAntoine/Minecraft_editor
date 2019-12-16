0780a56a96465c3aee6dbec1ee306065e7f15375
Antoine Libert <duncan-arrakis@hotmail.fr>
2019-12-16 09:57:04 +0100

    Correction of template setup in VertexBufferLayout

---
+++ Minecraft/src/VertexBufferLayout.cpp
@@ -0,0 +1,53 @@
+#include "VertexBufferLayout.h"
+
+namespace details {
+	template<typename T>
+	GLenum getGLenum() {
+		static_assert(false);
+	}
+
+	template<>
+	GLenum getGLenum<float>() {
+		return GL_FLOAT;
+	}
+
+	template<>
+	GLenum getGLenum<unsigned int>() {
+		return GL_UNSIGNED_INT;
+	}
+
+	template<>
+	GLenum getGLenum<int>() {
+		return GL_INT;
+	}
+
+	template<>
+	GLenum getGLenum<unsigned char>() {
+		return GL_UNSIGNED_BYTE;
+	}
+
+	template<typename T>
+	unsigned char getGLbool() {
+		static_assert(false);
+	}
+
+	template<>
+	unsigned char getGLbool<float>() {
+		return GL_FALSE;
+	}
+
+	template<>
+	unsigned char getGLbool<unsigned int>() {
+		return GL_FALSE;
+	}
+
+	template<>
+	unsigned char getGLbool<int>() {
+		return GL_FALSE;
+	}
+
+	template<>
+	unsigned char getGLbool<unsigned char>() {
+		return GL_TRUE;
+	}
+}
