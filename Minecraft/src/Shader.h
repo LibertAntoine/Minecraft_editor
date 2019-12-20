404d88cd48ea2ff534412aec033ee215526ea3ab
Nicolas LIENART <nicolas@lienart.net>
2019-12-20 18:30:20 +0100

    ADD SHADER.CPP unsigned int vector uniforms

--- Minecraft/src/Shader.h
+++ Minecraft/src/Shader.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <unordered_map>
+#include <GL/glew.h>
 #include "glm/glm.hpp"
 
 struct ShaderProgramSource {
@@ -24,6 +25,7 @@ public:
 
 	//Set Uniforms
 	void SetUniform1i(const std::string& name, int value);
+        void SetUniform1uiv(const std::string& name, GLuint* v1, int count);
 	void SetUniform1f(const std::string& name, float value);
 	void SetUniform3f(const std::string& name, float v0, float v1, float v2);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
