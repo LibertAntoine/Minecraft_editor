eca39b02d74189096170159f8945c81e773aa786
Nicolas LIENART <nicolas@lienart.net>
2019-12-25 21:18:20 +0100

    EDIT MISC simple code formating

--- Minecraft/src/VertexBufferLayout.h
+++ Minecraft/src/VertexBufferLayout.h
@@ -12,49 +12,54 @@ struct VertexBufferElement
   unsigned char normalized;
 
 
-	static unsigned int GetSizeOfType(unsigned int type)
-	{
-		switch (type)
-		{
-			case GL_FLOAT:			return 4;
-			case GL_UNSIGNED_INT:	return 4;
-			case GL_INT:			return 4;
-			case GL_UNSIGNED_BYTE:	return 1;
-		}
-		ASSERT(false);
-		return 0;
-	}
+  static unsigned int GetSizeOfType(unsigned int type)
+  {
+    switch (type)
+    {
+      case GL_FLOAT:			
+        return 4;
+      case GL_UNSIGNED_INT:
+        return 4;
+      case GL_INT:
+        return 4;
+      case GL_UNSIGNED_BYTE:
+        return 1;
+    }
+    ASSERT(false);
+    return 0;
+  }
 
 };
 
 
 namespace details {
-	template<typename T>
-	GLenum getGLenum();
+  template<typename T>
+    GLenum getGLenum();
 
-	template<typename T>
-	unsigned char getGLbool();
+  template<typename T>
+    unsigned char getGLbool();
 }
 
 
 
 class VertexBufferLayout
 {
-private:
-	std::vector<VertexBufferElement> m_Elements;
-	unsigned int m_Stride;
-public:
-	VertexBufferLayout()
-		: m_Stride(0) {};
+  private:
+    std::vector<VertexBufferElement> m_Elements;
+    unsigned int m_Stride;
+  public:
+    VertexBufferLayout()
+      : m_Stride(0) {};
 
 
-	template<typename T>
-	void Push(unsigned int count) {
-		m_Elements.push_back({ details::getGLenum<T>(), count, details::getGLbool<T>()});
-		m_Stride += count * VertexBufferElement::GetSizeOfType(details::getGLenum<T>());
-	}
-	
+    template<typename T>
+      void Push(unsigned int count)
+      {
+        m_Elements.push_back({ details::getGLenum<T>(), count, details::getGLbool<T>()});
+        m_Stride += count * VertexBufferElement::GetSizeOfType(details::getGLenum<T>());
+      }
 
-	inline const std::vector<VertexBufferElement> GetElements() const { return m_Elements; }
-	inline unsigned int GetStride() const { return m_Stride; }
+
+    inline const std::vector<VertexBufferElement> GetElements() const { return m_Elements; }
+    inline unsigned int GetStride() const { return m_Stride; }
 };
