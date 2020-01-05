a318ee3104d8a7e87e364a8ac1a18f8f3e91e408
Nicolas LIENART <nicolas@lienart.net>
2020-01-05 21:11:02 +0100

    COMMENTs FRAMEBUFFER comments

--- Minecraft/src/GraphicEngine/FrameBuffer.h
+++ Minecraft/src/GraphicEngine/FrameBuffer.h
@@ -10,11 +10,13 @@ class FrameBuffer {
     ~FrameBuffer();
     void Bind() const;
     void Unbind() const;
+
     GLuint GetFrameBufferId() const;
 
 		/**
 		 * \brief Extract pixel data from the current framebuffer
-		 * It 
+		 * \param attachment Specify on which attachment to pick the data from.
+		 * \param[out] data Array that will contain the extracted data
 		 */
 		void getDataAtPosition4ui(int x, int y, GLuint* data, GLenum attachment) const;
 		void getDataAtPosition4i(int x, int y, GLint* data, GLenum attachment) const;
